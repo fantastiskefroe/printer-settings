1d43bab67207632f544137d02d3d9d023b32aef3
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2023-06-16 18:06:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@ sudo rm /Library/Preferences/com.apple.print.customp*
 rm ~/Library/Preferences/com.apple.print.customp*
 
 cp com.apple.print.custompapers.plist ~/Library/Preferences
-sudo cp com.apple.print.custompapers.plist /Library/Preferences
 cp com.apple.print.custompresets.plist ~/Library/Preferences
-sudo cp com.apple.print.custompresets.plist /Library/Preferences
+
+# This doesn't seem to be necessary
+# sudo cp com.apple.print.custompapers.plist /Library/Preferences
+# sudo cp com.apple.print.custompresets.plist /Library/Preferences
+
 
