5de35d62d25d662c99d14d087bb4116d6f7b0b1e
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2023-06-16 18:04:28 +0200

    Install everywhere

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 sudo rm /Library/Preferences/com.apple.print.customp* 
 rm ~/Library/Preferences/com.apple.print.customp*
+
 cp com.apple.print.custompapers.plist ~/Library/Preferences
+sudo cp com.apple.print.custompapers.plist /Library/Preferences
 cp com.apple.print.custompresets.plist ~/Library/Preferences
+sudo cp com.apple.print.custompresets.plist /Library/Preferences
 
