71d8192ee3db6d32def866e954de373d04bf2cf5
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2023-06-16 18:20:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,8 @@
 sudo rm /Library/Preferences/com.apple.print.customp* 
 rm ~/Library/Preferences/com.apple.print.customp*
 
-curl https://github.com/fantastiskefroe/printer-settings/raw/main/com.apple.print.custompapers.plist \
-  --output ~/Library/Preferences/com.apple.print.custompapers.plist \
-  --silent
-curl https://github.com/fantastiskefroe/printer-settings/raw/main/com.apple.print.custompresets.plist \ 
-  --output ~/Library/Preferences/com.apple.print.custompresets.plist \
-  --silent
-
-#cp com.apple.print.custompapers.plist ~/Library/Preferences
-#cp com.apple.print.custompresets.plist ~/Library/Preferences
+curl "https://github.com/fantastiskefroe/printer-settings/raw/main/com.apple.print.custompapers.plist" -s -o ~/Library/Preferences/com.apple.print.custompapers.plist
+curl "https://github.com/fantastiskefroe/printer-settings/raw/main/com.apple.print.custompresets.plist" -s -o ~/Library/Preferences/com.apple.print.custompresets.plist 
 
 # This doesn't seem to be necessary
 # sudo cp com.apple.print.custompapers.plist /Library/Preferences
